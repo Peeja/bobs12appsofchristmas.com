a3bd5b6aff632654195c3204a27e9c556d31b39c
Peter Jaros <peter.a.jaros@gmail.com>
2009-12-23 22:45:06 -0500

    Initial commit.

---
+++ main.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+require 'haml'
+require 'activesupport'
+require 'linguistics'
+
+configure do
+  Linguistics.use(:en, :installProxy => :en)
+end
+
+
+get '/' do
+  day_of_christmas = 9
+  gifts = YAML.load_file("data/days.yml").sort_by(&:first).reverse.select { |day, _| day <= day_of_christmas }.map(&:last)
+  
+  # "and" hack
+  gifts.last["lyric"] = "and " + gifts.last["lyric"] if gifts.size > 1
+  
+  haml :index, :locals => { :gifts => gifts, :day_of_christmas => day_of_christmas }
+end
