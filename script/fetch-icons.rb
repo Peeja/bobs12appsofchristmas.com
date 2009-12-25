14ebf8b09056d6e2a3b8a44edd9472a2f8f28217
Peter Jaros <peter.a.jaros@gmail.com>
2009-12-24 23:36:17 -0500

    Fix fetch-icons.rb.

--- script/fetch-icons.rb
+++ script/fetch-icons.rb
@@ -2,12 +2,14 @@ require 'fileutils'
 require 'rubygems'
 require 'nokogiri'
 
-YAML.load_file("data/days.yml").map do |_, gift|
-  img_filename = File.dirname(__FILE__) + "/public" + gift['icon']
+root = File.dirname(__FILE__) + "/.."
+
+YAML.load_file(root + "/data/days.yml").each do |_, gift|
+  img_filename = root + "/public" + gift['icon']
   next if File.exist?(img_filename)
   
   xml = `curl -s -H "X-Apple-Store-Front: 143441-1,5" "#{gift["url"]}"`
-  img_url = Nokogiri::XML(xml).css("img.artwork").first['src']
+  img_url = Nokogiri::XML(xml).at_css("img.artwork")['src']
   FileUtils.mkdir_p(File.dirname(img_filename))
   system("curl -s -o #{img_filename} #{img_url}")
 end
